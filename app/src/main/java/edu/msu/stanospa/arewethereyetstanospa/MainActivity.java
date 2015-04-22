7c8686d3be43b77230b9fab58de29465d13cecc6
PStanos <paulstanos@gmail.com>
2015-04-21 20:23:37 -0400

    Completed

--- app/src/main/java/edu/msu/stanospa/arewethereyetstanospa/MainActivity.java
+++ app/src/main/java/edu/msu/stanospa/arewethereyetstanospa/MainActivity.java
@@ -2,7 +2,9 @@ package edu.msu.stanospa.arewethereyetstanospa;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.location.Address;
 import android.location.Criteria;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -11,13 +13,21 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 
 public class MainActivity extends ActionBarActivity {
 
     private LocationManager locationManager = null;
-    private ActiveListener activeListener = new ActiveListener();
+    private final ActiveListener activeListener = new ActiveListener();
 
     private double latitude = 0;
     private double longitude = 0;
@@ -42,12 +52,11 @@ public class MainActivity extends ActionBarActivity {
         toLatitude = Double.parseDouble(settings.getString(TOLAT, "42.724303"));
         toLongitude = Double.parseDouble(settings.getString(TOLONG, "-84.480507"));
 
-        latitude = 42.731138;
-        longitude = -84.487508;
-        valid = true;
-
         // Get the location manager
         locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+
+        // Force the screen to say on and bright
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
     }
 
     /**
@@ -117,6 +126,8 @@ public class MainActivity extends ActionBarActivity {
             locationManager.requestLocationUpdates(bestAvailable, 500, 1, activeListener);
             TextView viewProvider = (TextView)findViewById(R.id.textProvider);
             viewProvider.setText(bestAvailable);
+            Location location = locationManager.getLastKnownLocation(bestAvailable);
+            onLocation(location);
         }
     }
 
@@ -124,12 +135,150 @@ public class MainActivity extends ActionBarActivity {
         locationManager.removeUpdates(activeListener);
     }
 
+    private void onLocation(Location location) {
+        if(location == null) {
+            return;
+        }
+
+        latitude = location.getLatitude();
+        longitude = location.getLongitude();
+        valid = true;
+
+        savePreferences();
+
+        setUI();
+    }
+
+    private void savePreferences() {
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putString(TO, to);
+        editor.putString(TOLAT, String.valueOf(toLatitude));
+        editor.putString(TOLONG, String.valueOf(toLongitude));
+
+        editor.apply();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.itemSparty:
+                newTo("Sparty", 42.731138, -84.487508);
+                return true;
+
+            case R.id.itemHome:
+                newTo("Home", 42.726259, -84.469736);
+                return true;
+
+            case R.id.item1230:
+                newTo("1230 Engineering", 42.724303, -84.480507);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Handle setting a new "to" location.
+     * @param address Address to display
+     * @param lat latitude
+     * @param lon longitude
+     */
+    private void newTo(String address, double lat, double lon) {
+        to = address;
+        toLatitude = lat;
+        toLongitude = lon;
+
+        setUI();
+    }
+
+    public void onNew(View view) {
+        EditText location = (EditText)findViewById(R.id.editLocation);
+        final String address = location.getText().toString().trim();
+        newAddress(address);
+    }
+
+    private void newAddress(final String address) {
+        if(address.equals("")) {
+            // Don't do anything if the address is blank
+            return;
+        }
+
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                lookupAddress(address);
+
+            }
+
+        }).start();
+    }
+
+    /**
+     * Look up the provided address. This works in a thread!
+     * @param address Address we are looking up
+     */
+    private void lookupAddress(final String address) {
+        Geocoder geocoder = new Geocoder(MainActivity.this, Locale.US);
+        boolean exception = false;
+        List<Address> locations;
+        try {
+            locations = geocoder.getFromLocationName(address, 1);
+        } catch(IOException ex) {
+            // Failed due to I/O exception
+            locations = null;
+            exception = true;
+        }
+
+        final boolean ex = exception;
+        final List<Address> locs = locations;
+
+        runOnUiThread(new Runnable() {
+
+            @Override
+            public void run() {
+                newLocation(address, ex, locs);
+            }
+        });
+    }
+
+    private void newLocation(String address, boolean exception, List<Address> locations) {
+
+        if(exception) {
+            Toast.makeText(MainActivity.this, R.string.exception, Toast.LENGTH_SHORT).show();
+        } else {
+            if(locations == null || locations.size() == 0) {
+                Toast.makeText(this, R.string.couldnotfind, Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            EditText location = (EditText)findViewById(R.id.editLocation);
+            location.setText("");
+
+            // We have a valid new location
+            Address a = locations.get(0);
+            newTo(address, a.getLatitude(), a.getLongitude());
+
+        }
+    }
+
     private class ActiveListener implements LocationListener {
 
 
         @Override
         public void onLocationChanged(Location location) {
-
+            onLocation(location);
         }
 
         @Override
@@ -144,7 +293,7 @@ public class MainActivity extends ActionBarActivity {
 
         @Override
         public void onProviderDisabled(String provider) {
-
+            registerListeners();
         }
-    };
+    }
 }
